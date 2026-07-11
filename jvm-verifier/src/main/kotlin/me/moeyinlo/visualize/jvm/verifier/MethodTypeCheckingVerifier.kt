d220142f6819225a496ce42ddfdf258778d11e9a
xiguajerry <xiguajerry@outlook.com>
2026-07-12 07:01:54 +0800

    feat(verifier): type check method char array loads

--- jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
+++ jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
@@ -279,6 +279,10 @@ object MethodTypeCheckingVerifier {
                     frame = frame,
                     maxStack = code.maxStack,
                 )
+                0x34 -> CharArrayLoadInstructionVerifier.verify(
+                    frame = frame,
+                    maxStack = code.maxStack,
+                )
                 0x36 -> LocalStoreInstructionVerifier.verify(
                     frame = frame,
                     index = code.code.u1(offset + 1),
