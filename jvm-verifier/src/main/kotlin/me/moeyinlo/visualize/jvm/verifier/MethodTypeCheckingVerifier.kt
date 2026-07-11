d7bed18e8d17c752422285fad9488105ad5005f5
xiguajerry <xiguajerry@outlook.com>
2026-07-12 07:11:13 +0800

    feat(verifier): type check method long array stores

--- jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
+++ jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
@@ -361,6 +361,10 @@ object MethodTypeCheckingVerifier {
                     frame = frame,
                     maxStack = code.maxStack,
                 )
+                0x50 -> LongArrayStoreInstructionVerifier.verify(
+                    frame = frame,
+                    maxStack = code.maxStack,
+                )
                 in 0x99..0x9E -> IntZeroBranchInstructionVerifier.verify(
                     frame = frame,
                     maxStack = code.maxStack,
