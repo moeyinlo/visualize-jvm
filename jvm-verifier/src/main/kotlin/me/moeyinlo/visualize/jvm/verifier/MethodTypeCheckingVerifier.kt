e2c52b2c6eac443b4d191f5214dee25b4ce6ccf0
xiguajerry <xiguajerry@outlook.com>
2026-07-12 07:04:26 +0800

    feat(verifier): type check method short array loads

--- jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
+++ jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
@@ -283,6 +283,10 @@ object MethodTypeCheckingVerifier {
                     frame = frame,
                     maxStack = code.maxStack,
                 )
+                0x35 -> ShortArrayLoadInstructionVerifier.verify(
+                    frame = frame,
+                    maxStack = code.maxStack,
+                )
                 0x36 -> LocalStoreInstructionVerifier.verify(
                     frame = frame,
                     index = code.code.u1(offset + 1),
