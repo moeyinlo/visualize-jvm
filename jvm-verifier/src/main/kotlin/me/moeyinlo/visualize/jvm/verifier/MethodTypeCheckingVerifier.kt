7de692cf743d4b4f5cb6527f2de405b9ecd6dec9
xiguajerry <xiguajerry@outlook.com>
2026-07-12 06:59:26 +0800

    feat(verifier): type check method byte array loads

--- jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
+++ jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
@@ -275,6 +275,10 @@ object MethodTypeCheckingVerifier {
                     frame = frame,
                     maxStack = code.maxStack,
                 )
+                0x33 -> ByteArrayLoadInstructionVerifier.verify(
+                    frame = frame,
+                    maxStack = code.maxStack,
+                )
                 0x36 -> LocalStoreInstructionVerifier.verify(
                     frame = frame,
                     index = code.code.u1(offset + 1),
