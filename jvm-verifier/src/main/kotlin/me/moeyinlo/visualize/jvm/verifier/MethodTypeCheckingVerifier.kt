2654e1e585010285b44103902a517121b51ccde5
xiguajerry <xiguajerry@outlook.com>
2026-07-12 06:31:58 +0800

    feat(verifier): type check wide long local stores

--- jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
+++ jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
@@ -191,6 +191,13 @@ object MethodTypeCheckingVerifier {
                         maxLocals = code.maxLocals,
                         maxStack = code.maxStack,
                     )
+                    0x37 -> WideLocalStoreInstructionVerifier.verify(
+                        frame = frame,
+                        index = code.code.u2(offset + 2),
+                        kind = LocalStoreKind.Long,
+                        maxLocals = code.maxLocals,
+                        maxStack = code.maxStack,
+                    )
                 }
                 in 0x1A..0x1D -> LocalLoadInstructionVerifier.verify(
                     frame = frame,
