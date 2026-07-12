5deb08e0ac57a933e37b5b7f5e86220f5f5dd50b
xiguajerry <xiguajerry@outlook.com>
2026-07-12 11:36:47 +0800

    feat(verifier): type check method lcmp

--- jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
+++ jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
@@ -666,6 +666,10 @@ object MethodTypeCheckingVerifier {
                     frame = frame,
                     maxStack = code.maxStack,
                 )
+                0x94 -> LongCompareInstructionVerifier.verify(
+                    frame = frame,
+                    maxStack = code.maxStack,
+                )
                 in 0x99..0x9E -> IntZeroBranchInstructionVerifier.verify(
                     frame = frame,
                     maxStack = code.maxStack,
