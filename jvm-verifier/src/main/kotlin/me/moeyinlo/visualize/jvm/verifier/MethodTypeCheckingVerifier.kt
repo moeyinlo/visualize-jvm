dc63b01ec96910da0c2a409a10df482f3de86a1f
xiguajerry <xiguajerry@outlook.com>
2026-07-12 10:50:14 +0800

    feat(verifier): type check method l2d

--- jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
+++ jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/MethodTypeCheckingVerifier.kt
@@ -626,6 +626,10 @@ object MethodTypeCheckingVerifier {
                     frame = frame,
                     maxStack = code.maxStack,
                 )
+                0x8A -> LongToDoubleConversionInstructionVerifier.verify(
+                    frame = frame,
+                    maxStack = code.maxStack,
+                )
                 in 0x99..0x9E -> IntZeroBranchInstructionVerifier.verify(
                     frame = frame,
                     maxStack = code.maxStack,
