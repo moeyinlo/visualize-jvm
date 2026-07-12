93359e32178cf4264fafb3cafcc32920166b599d
xiguajerry <xiguajerry@outlook.com>
2026-07-12 12:34:30 +0800

    feat(verifier): type check method ldc methodhandle

--- jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/LdcInstructionVerifier.kt
+++ jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/LdcInstructionVerifier.kt
@@ -3,6 +3,7 @@ package me.moeyinlo.visualize.jvm.verifier
 import me.moeyinlo.visualize.jvm.classfile.ConstantClassEntry
 import me.moeyinlo.visualize.jvm.classfile.ConstantFloatEntry
 import me.moeyinlo.visualize.jvm.classfile.ConstantIntegerEntry
+import me.moeyinlo.visualize.jvm.classfile.ConstantMethodHandleEntry
 import me.moeyinlo.visualize.jvm.classfile.ConstantMethodTypeEntry
 import me.moeyinlo.visualize.jvm.classfile.ConstantPool
 import me.moeyinlo.visualize.jvm.classfile.ConstantPoolFormatException
@@ -22,6 +23,7 @@ object LdcInstructionVerifier {
             is ConstantFloatEntry -> VerificationType.Float
             is ConstantStringEntry -> VerificationType.ClassType("java/lang/String")
             is ConstantClassEntry -> VerificationType.ClassType("java/lang/Class")
+            is ConstantMethodHandleEntry -> VerificationType.ClassType("java/lang/invoke/MethodHandle")
             is ConstantMethodTypeEntry -> VerificationType.ClassType("java/lang/invoke/MethodType")
             else -> throw MethodVerificationException(
                 "ldc constant_pool index $index references unsupported constant ${entry.javaClass.simpleName}",
