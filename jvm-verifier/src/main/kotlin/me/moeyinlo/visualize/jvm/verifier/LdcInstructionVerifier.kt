aeb010d9ee07323447d8234094fcbdebee5ecf46
xiguajerry <xiguajerry@outlook.com>
2026-07-12 13:01:59 +0800

    feat(verifier): type check method ldc dynamic boolean

--- jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/LdcInstructionVerifier.kt
+++ jvm-verifier/src/main/kotlin/me/moeyinlo/visualize/jvm/verifier/LdcInstructionVerifier.kt
@@ -81,6 +81,7 @@ object LdcInstructionVerifier {
     ): VerificationType {
         return when (val descriptor = dynamicConstantDescriptor(entry = entry, constantPool = constantPool)) {
             "I" -> VerificationType.Integer
+            "Z" -> VerificationType.Integer
             "F" -> VerificationType.Float
             else -> throw MethodVerificationException(
                 "ldc CONSTANT_Dynamic descriptor '$descriptor' is unsupported",
