3b52dcf740a82ea7f41b6b0b2521522cfe77d10e
xiguajerry <xiguajerry@outlook.com>
2026-07-18 21:14:10 +0800

    feat(interpreter): throw guest aioobe for lastore bounds

--- jvm-interpreter/src/test/kotlin/me/moeyinlo/visualize/jvm/interpreter/JvmInterpreterTest.kt
+++ jvm-interpreter/src/test/kotlin/me/moeyinlo/visualize/jvm/interpreter/JvmInterpreterTest.kt
@@ -4535,6 +4535,31 @@ class JvmInterpreterTest {
     }
 
     @Test
+    fun `lastore throws guest ArrayIndexOutOfBoundsException for out of range index`() {
+        val heap = JvmHeap()
+        val reference = heap.allocateLongArray(3)
+        val locals = JvmLocalVariables(maxLocals = 1)
+        locals.store(0, reference)
+
+        val exception = assertFailsWith<JvmArrayIndexOutOfBoundsException> {
+            JvmInterpreter.execute(
+                code = byteArrayOf(
+                    0x2A.toByte(),
+                    0x06.toByte(),
+                    0x0A.toByte(),
+                    0x50.toByte(),
+                ),
+                maxStack = 4,
+                heap = heap,
+                localVariables = locals,
+            )
+        }
+
+        assertEquals("java/lang/ArrayIndexOutOfBoundsException", exception.guestClassName)
+        assertEquals("lastore index 3 out of bounds for length 3", exception.message)
+    }
+
+    @Test
     fun `fastore stores a float into a float array`() {
         val heap = JvmHeap()
         val reference = heap.allocateFloatArray(3)
