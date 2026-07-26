1ac998f24abf916adab4e09640b5103b194b5ebc
xiguajerry <xiguajerry@outlook.com>
2026-07-26 14:56:29 +0800

    feat(runtime): carry invokedynamic special method constant pools

--- jvm-interpreter/src/main/kotlin/me/moeyinlo/visualize/jvm/interpreter/JvmInterpreter.kt
+++ jvm-interpreter/src/main/kotlin/me/moeyinlo/visualize/jvm/interpreter/JvmInterpreter.kt
@@ -6198,7 +6198,7 @@ object JvmInterpreter {
         val frameResult = executeFrame(
             code = methodCode,
             maxStack = targetMethod.maxStack,
-            constantPool = constantPool,
+            constantPool = targetMethod.constantPool ?: constantPool,
             heap = heap,
             localVariables = calleeLocals,
             classHierarchy = classHierarchy,
