f5836cbc1b5321ed643a83b49b92b4450bc269ae
xiguajerry <xiguajerry@outlook.com>
2026-07-29 21:31:30 +0800

    test(interpreter): cover loaded runtime package for anewarray

--- jvm-interpreter/src/test/kotlin/me/moeyinlo/visualize/jvm/interpreter/JvmInterpreterTest.kt
+++ jvm-interpreter/src/test/kotlin/me/moeyinlo/visualize/jvm/interpreter/JvmInterpreterTest.kt
@@ -5048,6 +5048,58 @@ class JvmInterpreterTest {
     }
 
     @Test
+    fun `anewarray rejects package private component classes from same named package in different defining loaders`() {
+        val appLoader = JvmClassLoaderIdentity.UserDefined(id = 59, displayName = "app")
+        val libraryLoader = JvmClassLoaderIdentity.UserDefined(id = 60, displayName = "library")
+        val callerKey = JvmLoadedClassKey("pkg/Caller", appLoader)
+        val hiddenKey = JvmLoadedClassKey("pkg/Hidden", libraryLoader)
+        val methodArea = JvmMethodArea()
+        methodArea.defineClass(
+            JvmMethodAreaEntry(
+                definition = JvmClassDefinition("pkg/Caller"),
+                loadedClassKey = callerKey,
+            ),
+        )
+        methodArea.defineClass(
+            JvmMethodAreaEntry(
+                definition = JvmClassDefinition("pkg/Hidden", isPublic = false),
+                loadedClassKey = hiddenKey,
+                initiatingLoaders = setOf(appLoader),
+            ),
+        )
+
+        val exception = assertFailsWith<JvmIllegalAccessError> {
+            JvmInterpreter.execute(
+                code = byteArrayOf(
+                    0x06.toByte(),
+                    0xBD.toByte(),
+                    0x00.toByte(),
+                    0x02.toByte(),
+                ),
+                maxStack = 1,
+                constantPool = ConstantPool.fromEntries(
+                    listOf(
+                        ConstantUtf8Entry("pkg/Hidden", "pkg/Hidden".encodeToByteArray()),
+                        ConstantClassEntry(ConstantPoolIndex(1)),
+                    ),
+                ),
+                classHierarchy = JvmClassHierarchy(
+                    listOf(
+                        JvmClassDefinition("pkg/Hidden", isPublic = false),
+                        JvmClassDefinition("pkg/Caller"),
+                    ),
+                ),
+                currentClassName = "pkg/Caller",
+                currentLoadedClassKey = callerKey,
+                methodArea = methodArea,
+            )
+        }
+
+        assertEquals("java/lang/IllegalAccessError", exception.guestClassName)
+        assertEquals("Class pkg/Caller cannot access class pkg/Hidden", exception.message)
+    }
+
+    @Test
     fun `newarray allocates an int array with default zero values`() {
         val heap = JvmHeap()
         val result = JvmInterpreter.execute(
