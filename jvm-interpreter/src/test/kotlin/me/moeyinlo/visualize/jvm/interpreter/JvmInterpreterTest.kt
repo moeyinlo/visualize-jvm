f51cae2f66f6c3e5137da9423e05f7351cb40b0b
xiguajerry <xiguajerry@outlook.com>
2026-07-29 22:35:04 +0800

    test(interpreter): cover loaded runtime package for invokeinterface owner access

--- jvm-interpreter/src/test/kotlin/me/moeyinlo/visualize/jvm/interpreter/JvmInterpreterTest.kt
+++ jvm-interpreter/src/test/kotlin/me/moeyinlo/visualize/jvm/interpreter/JvmInterpreterTest.kt
@@ -29229,6 +29229,85 @@ class JvmInterpreterTest {
         assertEquals("Class other/Caller cannot access class pkg/Face", exception.message)
     }
     @Test
+    fun `invokeinterface rejects package private owner classes from same named package in different defining loaders`() {
+        val appLoader = JvmClassLoaderIdentity.UserDefined(id = 123, displayName = "app")
+        val libraryLoader = JvmClassLoaderIdentity.UserDefined(id = 124, displayName = "library")
+        val callerKey = JvmLoadedClassKey("pkg/Caller", appLoader)
+        val faceKey = JvmLoadedClassKey("pkg/Face", libraryLoader)
+        val implKey = JvmLoadedClassKey("pkg/Impl", libraryLoader)
+        val methodArea = JvmMethodArea()
+        methodArea.defineClass(
+            JvmMethodAreaEntry(
+                definition = JvmClassDefinition("pkg/Caller"),
+                loadedClassKey = callerKey,
+            ),
+        )
+        methodArea.defineClass(
+            JvmMethodAreaEntry(
+                definition = JvmClassDefinition("pkg/Face", isPublic = false, isInterface = true),
+                loadedClassKey = faceKey,
+                initiatingLoaders = setOf(appLoader),
+            ),
+        )
+        methodArea.defineClass(
+            JvmMethodAreaEntry(
+                definition = JvmClassDefinition("pkg/Impl", interfaceNames = listOf("pkg/Face")),
+                loadedClassKey = implKey,
+                initiatingLoaders = setOf(appLoader),
+            ),
+        )
+        val heap = JvmHeap()
+        val receiver = heap.allocateObject("pkg/Impl")
+        val callerLocals = JvmLocalVariables(maxLocals = 1)
+        callerLocals.store(0, receiver)
+
+        val exception = assertFailsWith<JvmIllegalAccessError> {
+            JvmInterpreter.execute(
+                code = byteArrayOf(
+                    0x2A.toByte(),
+                    0x0A.toByte(),
+                    0xB9.toByte(),
+                    0x00.toByte(),
+                    0x01.toByte(),
+                    0x03.toByte(),
+                    0x00.toByte(),
+                ),
+                maxStack = 3,
+                constantPool = interfaceMethodConstantPool(),
+                heap = heap,
+                localVariables = callerLocals,
+                classHierarchy = JvmClassHierarchy(
+                    listOf(
+                        JvmClassDefinition(
+                            internalName = "pkg/Face",
+                            isPublic = false,
+                            isInterface = true,
+                            methods = listOf(
+                                JvmMethodDefinition(
+                                    name = "value",
+                                    descriptor = "(J)I",
+                                    isStatic = false,
+                                    isAbstract = true,
+                                ),
+                            ),
+                        ),
+                        JvmClassDefinition(
+                            internalName = "pkg/Impl",
+                            interfaceNames = listOf("pkg/Face"),
+                        ),
+                        JvmClassDefinition(internalName = "pkg/Caller"),
+                    ),
+                ),
+                currentClassName = "pkg/Caller",
+                currentLoadedClassKey = callerKey,
+                methodArea = methodArea,
+            )
+        }
+
+        assertEquals("java/lang/IllegalAccessError", exception.guestClassName)
+        assertEquals("Class pkg/Caller cannot access class pkg/Face", exception.message)
+    }
+    @Test
     fun `invokeinterface rejects package private interface methods from same named package in different defining loaders`() {
         val appLoader = JvmClassLoaderIdentity.UserDefined(id = 63, displayName = "app")
         val libraryLoader = JvmClassLoaderIdentity.UserDefined(id = 64, displayName = "library")
